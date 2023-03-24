6a62c04d30267442d36943100bfe3b4105c1c9a7
Chris Wendt <chris.wendt@outlook.com>
2023-03-23 17:52:21 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+using HtmlAgilityPack;
+using System.Xml;
+
+Console.WriteLine("Extract plain text from HTML.");
+
+
+
+static string ExtractText(string html)
+{
+    if (html == null)
+    {
+        throw new ArgumentNullException(nameof(html));
+    }
+
+    HtmlDocument doc = new();
+    doc.LoadHtml(html);
+
+    var chunks = new List<string>();
+
+    foreach (var item in doc.DocumentNode.DescendantsAndSelf())
+    {
+        if (item.NodeType == HtmlNodeType.Text)
+        {
+            if (item.InnerText.Trim() != "")
+            {
+                chunks.Add(item.InnerText.Trim());
+            }
+        }
+    }
+    return String.Join(" ", chunks);
+}

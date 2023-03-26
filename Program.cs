2bbe6705da0ad6815db374aeba0e2e6e622e8075
Chris Wendt <chris.wendt@outlook.com>
2023-03-25 19:22:15 -0700

    Extract all text nodes

--- Program.cs
+++ Program.cs
@@ -1,11 +1,47 @@
-﻿// See https://aka.ms/new-console-template for more information
-using HtmlAgilityPack;
+﻿using HtmlAgilityPack;
+
 
 Console.WriteLine("Extract plain text from HTML.");
+if (args.Length > 0)
+{
+    string rootPath = args[0];
+    TraverseDirectory(rootPath, ProcessFile);
+    return 0;
+}
+else
+    Console.WriteLine("ERROR: Please provide a folder path as argument.");
+return 1;
+
+
+void TraverseDirectory(string path, Action<string> action)
+{
+    foreach (string file in Directory.GetFiles(path))
+    {
+        if (file.ToLowerInvariant().EndsWith(".htm") || file.ToLowerInvariant().EndsWith("html"))
+        {
+            action(file);
+        }
+    }
+
+    foreach (string directory in Directory.GetDirectories(path))
+    {
+        TraverseDirectory(directory, action);
+    }
+}
+
+void ProcessFile(string filePath)
+{
+    string html = File.ReadAllText(filePath);
+    Console.WriteLine($"Processing file: {filePath} {html.Length} characters");
+    string result = ExtractText(html);
+    string newFileName = filePath + ".txt";
+    File.WriteAllText(newFileName, result);
+    Console.WriteLine($"Plain text written to: {newFileName} {result.Length} characters");
+}
 
 
 
-static string ExtractText(string html)
+string ExtractText(string html)
 {
     if (html == null)
     {
